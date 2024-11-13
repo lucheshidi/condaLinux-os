422cd95e9bbc3ee0038b55c6212dbc89b2feb495
lucheshidi <lucheshidi@163.com>
2024-11-13 09:01:10 +0100

    init recorded

--- libs/PSCP/README.rst
+++
@@ -1,6 +0,0 @@
-# Example Package
-This is a simple example package. You can use
-
-[call author](https://github.com/lucheshidi)
-
-to write your content.
--- libs/PSCP/__init__.py
+++ libs/PSCP/__init__.py
@@ -1,6 +1,22 @@
 from sys import *
 from random import *
+import platform
 import os
 
 path = str(os.getcwd())
-print(path)
+
+# check cpu
+def cpuChecker(best_cpu, worst_cpu):
+    machine_cpu = platform.processor()
+    print(machine_cpu)
+    return machine_cpu
+
+cpuChecker("i5")
+
+# check Memory
+def memoryChecker(max, mix):
+    pass
+
+# check disk
+def diskChecker(max, mix):
+    pass
