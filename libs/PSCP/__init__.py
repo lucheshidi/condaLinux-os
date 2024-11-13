cef62b4243f15f96abd7fbc9de0af075586f46da
lucheshidi <lucheshidi@163.com>
2024-11-13 15:15:24 +0100

    init recorded

--- libs/PSCP/__init__.py
+++ libs/PSCP/__init__.py
@@ -1,21 +1,40 @@
 from sys import *
 from random import *
+from subprocess import *
 import platform
 import os
 
+def run_command(command):
+    result = subprocess.run(command, shell=True, text=True)
+    return result
+
+run_command("python -m pip install --upgrade pip")
+run_command("python -m pip install psutil")
+
+import psutil
+
+mem = psutil.virtual_memory()
 path = str(os.getcwd())
+
+# print ver
+def printVer():
+    print("System Check Program v1.15.33")
+    print("Preparing to check ...")
+    time.sleep()
 
 # check cpu
 def cpuChecker(best_cpu, worst_cpu):
     machine_cpu = platform.processor()
-    print(machine_cpu)
-    return machine_cpu
-
-cpuChecker("i5")
+    if best_cpu in machine_cpu or worst_cpu in machine_cpu:
+        return False
+    else:
+        return True
 
 # check Memory
-def memoryChecker(max, mix):
-    pass
+def memoryChecker(total, available):
+    total_memory = mem.total / (1024 ** 3)
+    used_memory = mem.used / (1024 ** 3)
+    available_memory = mem.available / (1024 ** 3)
 
 # check disk
 def diskChecker(max, mix):
---
+++ test.py
@@ -0,0 +1,5 @@
+# -*- coding:utf-8 -*-
+# name:----------Jyx
+# time:----------2023.1.15
+
+import SCM
